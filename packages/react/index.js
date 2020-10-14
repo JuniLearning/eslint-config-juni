f7d41938a9b872f7cb97990fd050ac3b268c1c7a
Julia Proft <jmproft@gmail.com>
2020-10-13 22:32:04 -0600

    Use @babel/eslint-parser instead of babel-eslint

--- packages/react/index.js
+++ packages/react/index.js
@@ -8,13 +8,15 @@ module.exports = {
     './rules/jsx-a11y.js',
     '../base',
   ].map(require.resolve),
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2020,
     ecmaFeatures: {
       impliedStrict: true,
       classes: true,
     },
+    requireConfigFile: false,
   },
+  plugins: ['@babel'],
   rules: {},
 };
