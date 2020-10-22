79968f58f14d9d167e6984eb95f4a06efb4b563d
Tristan Tarpley <tristan@learnwithjuni.com>
2020-10-22 10:56:37 -0500

    add typescript folder

---
+++ packages/typescript/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "./rules/styles",
+  ]
+}
---
+++ packages/typescript/rules/styles.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 0,
+  },
+};
