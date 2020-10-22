c479b8da6adb25b156cf18af0b597a34e8f0a454
Tristan Tarpley <tristan@learnwithjuni.com>
2020-10-22 11:20:55 -0500

    typescript no-use-before-define

--- packages/typescript/rules/styles.js
+++ packages/typescript/rules/styles.js
@@ -2,5 +2,8 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 2,
   },
 };
