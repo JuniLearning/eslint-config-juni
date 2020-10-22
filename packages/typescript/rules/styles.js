dd716f65e8d849598a4fba6fefe71b4b2866d114
Tristan Tarpley <tristan@learnwithjuni.com>
2020-10-22 15:59:45 -0500

    typescript/no-unused-expressions

--- packages/typescript/rules/styles.js
+++ packages/typescript/rules/styles.js
@@ -6,5 +6,8 @@ module.exports = {
     'no-use-before-define': 0,
     '@typescript-eslint/no-use-before-define': 2,
     '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    'no-unused-expressions': 0,
+    '@typescript-eslint/no-unused-expressions': 2,
   },
 };
