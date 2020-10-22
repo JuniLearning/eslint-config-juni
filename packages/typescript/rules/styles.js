4c2547146b813a311f09c2abe2ba286ae2476afe
Tristan Tarpley <tristan@learnwithjuni.com>
2020-10-22 11:29:48 -0500

    disable no-non-null-assertion

--- packages/typescript/rules/styles.js
+++ packages/typescript/rules/styles.js
@@ -5,5 +5,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 0,
     'no-use-before-define': 0,
     '@typescript-eslint/no-use-before-define': 2,
+    '@typescript-eslint/no-non-null-assertion': 0,
   },
 };
